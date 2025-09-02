d978282cb6d03ba153b7755aa7395494025f704e
SacalF <frislymanuel07@gmail.com>
2025-09-01 22:03:21 -0600

    Segunda version

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Lista Tarea',
+  description: 'Lista de tareas para',
+  generator: 'v0.1.0',
 }
 
 export default function RootLayout({
